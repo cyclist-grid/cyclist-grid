f19c8b74ac6f7db9a8fbada80defa832ae357df1
Roben Kleene <robenk@gmail.com>
2012-11-09 23:15:08 -0500

    Working on drawing baseline grid

--- js/grid.js
+++ js/grid.js
@@ -1,53 +1,52 @@
 window.addEventListener('load', function () { grid(); }, false);
 
-function drawGoldenOnCanvas(canvas) {
-	if (canvas && canvas.getContext) {
-		var context = canvas.getContext('2d');
-		if (context) {
-			context.fillRect(0, 0, 150, 100);
-		}
+function drawBaselineOnCanvas(canvas, lineHeight) {	
+	var context = contextForCanvas(canvas);
+	if (context) {
+		// Border
+		context.strokeStyle = "#f00";
+		context.lineWidth = 1;
+		context.strokeRect(0, 0, canvas.offsetWidth, canvas.offsetHeight);
+		// Baseline
+		context.strokeStyle = "#00f";
+		
 	}
 }
 
-// Function that returns a canvas for the designated element
-// E.g.:
-// function canvasForElement(element)
-
 function grid() {
-	var baselineTags = document.getElementsByClassName("baseline");
-	for (var i=0; i < baselineTags.length; i++) {
-
-		var baselineTag = baselineTags[i];
-		var canvas = canvasForElement(baselineTag);
-		// baselineTag.style.position = "relative";
-		// 
-		// var baselineCanvas = document.createElement("canvas");
-		// baselineCanvas.style.width = "100%";
-		// baselineCanvas.style.height = "100%";
-		// // goldenCanvas.style.backgroundColor = "black";
-		// baselineCanvas.style.position = "absolute";
-		// baselineCanvas.style.left = "0";
-		// baselineCanvas.style.top = "0";
-		// 
-		// baselineTag.appendChild(baselineCanvas);
-		drawGoldenOnCanvas(canvas);
+	var baselineElements = document.getElementsByClassName("baseline");
+	for (var i=0; i < baselineElements.length; i++) {
+		var baselineElement = baselineElements[i];
+		var canvas = canvasForElement(baselineElement);
+        var lineHeight = window.getComputedStyle(baselineElement, null).getPropertyValue("line-height");
+		drawBaselineOnCanvas(canvas, lineHeight);
 	};
 }
 
 // Helpers
+
+function contextForCanvas(canvas) {
+	if (canvas && canvas.getContext) {
+		return canvas.getContext('2d');
+	}
+}
 
 function canvasForElement(element) {
 
 	element.style.position = "relative";
 
 	var canvas = document.createElement("canvas");
-	canvas.style.width = "100%";
-	canvas.style.height = "100%";
+
+	// with jQuerty I could replace this http://stackoverflow.com/questions/1520132/how-to-find-actual-rendered-values-of-elements-set-to-auto-using-javascript
+	canvas.setAttribute("width", element.offsetWidth);
+	canvas.setAttribute("height", element.offsetHeight);
+
 	// canvas.style.backgroundColor = "black";
 	canvas.style.position = "absolute";
 	canvas.style.left = "0";
 	canvas.style.top = "0";
 
 	element.appendChild(canvas);
+
 	return canvas;
 }
