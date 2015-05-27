e33e59a53071a2c1e093fa7f1a4666751b941ad8
Roben Kleene <robenk@gmail.com>
2015-05-26 21:55:50 -0400

    Add gulp path variables

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,35 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var path = require('path');
 
-gulp.task('sass', function () {
-  gulp.src('./examples/src/sass/*.scss')
+// Paths
+var paths = {
+    test: {
+      src: './test/src/',
+      build: './test/build/'
+    },
+    examples: {
+      src: './examples/src/',
+      build: './examples/build/'
+    }
+};
+
+
+// Sass
+
+gulp.task('sass-examples', function () {
+  var srcPath = path.join(paths.examples.src, 'sass/*.scss');
+  var buildPath = path.join(paths.examples.build, 'css');
+  gulp.src(srcPath)
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./examples/build/css'));
+    .pipe(gulp.dest(buildPath));
 });
+
 
 gulp.task('watch', function () {
   gulp.watch('./examples/src/sass/*.scss', ['sass']);
 });
 
+gulp.task('sass', ['sass-examples']);
 gulp.task('default', ['sass']);
