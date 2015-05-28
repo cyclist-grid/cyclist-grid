e297e044ae1628ecf7daa50af7b8a3a2265c5548
Roben Kleene <robenk@gmail.com>
2015-05-27 20:35:53 -0400

    Refactor gulpfile paths

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,48 @@
-'use strict';
-
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var path = require('path');
 
 // Paths
-var paths = {
-    test: {
-      src: './test/src/',
-      build: './test/build/'
-    },
-    examples: {
-      src: './examples/src/',
-      build: './examples/build/'
-    }
-};
+function makePaths(basePath) {
+  var paths = {};
+  var srcPath = path.join(basePath, 'src');
+  var buildPath = path.join(basePath, 'build');
+  paths.src = srcPath;
+  paths.build = buildPath;
+  // Sass
+  paths.sass = {};
+  paths.sass.src = path.join(srcPath, 'sass');
+  paths.sass.build = path.join(buildPath, 'css');
+  return paths;
+}
+var paths = {};
+paths.examples = makePaths('example');
+paths.test = makePaths('test');
 
 
 // Sass
 
-gulp.task('sass-examples', function () {
-  var srcPath = path.join(paths.examples.src, 'sass/*.scss');
-  var buildPath = path.join(paths.examples.build, 'css');
+function doSass(srcPath, buildPath) {
   gulp.src(srcPath)
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest(buildPath));
+}
+
+gulp.task('sass-examples', function () {
+  doSass(paths.examples.sass.src + '/*.scss', paths.examples.sass.build);
 });
 
+gulp.task('sass-test', function () {
+  doSass(paths.test.sass.src + '/*.scss', paths.test.sass.build);
+});
+
+
+// Watch
 
 gulp.task('watch', function () {
-  gulp.watch('./examples/src/sass/*.scss', ['sass']);
+  gulp.watch(paths.examples.sass.src + '/*.scss', ['sass-examples']);
+  gulp.watch(paths.test.sass.src + '/*.scss', ['sass-test']);
 });
 
-gulp.task('sass', ['sass-examples']);
+gulp.task('sass', ['sass-examples', 'sass-test']);
 gulp.task('default', ['sass']);
