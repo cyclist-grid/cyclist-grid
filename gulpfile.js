34c98584125306543ea9003bd4e4652f7f6b54cc
Roben Kleene <robenk@gmail.com>
2015-05-27 21:07:22 -0400

    Setup jade

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var sass = require('gulp-sass');
 var path = require('path');
 
 // Paths
+
 function makePaths(basePath) {
   var paths = {};
   var srcPath = path.join(basePath, 'src');
@@ -13,12 +14,18 @@ function makePaths(basePath) {
   paths.sass = {};
   paths.sass.src = path.join(srcPath, 'sass');
   paths.sass.build = path.join(buildPath, 'css');
+  paths.sass.srcGlob = paths.sass.src + '/*.scss';
+  // Jade
+  paths.jade = {};
+  paths.jade.src = path.join(srcPath, 'jade');
+  paths.jade.build = path.join(buildPath, 'html');
+  paths.jade.srcGlob = paths.jade.src + '/*.jade';
+
   return paths;
 }
 var paths = {};
 paths.examples = makePaths('example');
 paths.test = makePaths('test');
-
 
 // Sass
 
@@ -29,20 +36,36 @@ function doSass(srcPath, buildPath) {
 }
 
 gulp.task('sass-examples', function () {
-  doSass(paths.examples.sass.src + '/*.scss', paths.examples.sass.build);
+  doSass(paths.examples.sass.srcGlob, paths.examples.sass.build);
 });
 
 gulp.task('sass-test', function () {
-  doSass(paths.test.sass.src + '/*.scss', paths.test.sass.build);
+  doSass(paths.test.sass.srcGlob, paths.test.sass.build);
 });
 
+// Jade
+
+function doJade(srcPath, buildPath) {
+  gulp.src(srcPath)
+    .pipe(plumber())
+    .pipe(jade({
+      pretty: true
+    }))
+    .pipe(gulp.dest(buildPath));
+}
+
+gulp.task('jade-test', function() {
+  doJade(paths.test.jade.srcGlob, paths.test.jade.build);
+});
 
 // Watch
 
 gulp.task('watch', function () {
-  gulp.watch(paths.examples.sass.src + '/*.scss', ['sass-examples']);
-  gulp.watch(paths.test.sass.src + '/*.scss', ['sass-test']);
+  gulp.watch(paths.examples.sass.srcGlob, ['sass-examples']);
+  gulp.watch(paths.test.sass.srcGlob, ['sass-test']);
+  gulp.watch(paths.test.jade.srcGlob, ['jade-test']);
 });
 
+gulp.task('jade', ['jade-test']);
 gulp.task('sass', ['sass-examples', 'sass-test']);
-gulp.task('default', ['sass']);
+gulp.task('default', ['sass', 'jade']);
