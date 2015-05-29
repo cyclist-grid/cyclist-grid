8803310dbc0e66b058719f77f68a0a59b9191999
Roben Kleene <robenk@gmail.com>
2015-05-28 20:58:31 -0400

    Setup test printing style of elements

--- test/lib/tests/test-basic.js
+++ test/lib/tests/test-basic.js
@@ -1,8 +1,22 @@
+// TODO: Iterate over every child of the `#content` and display it's height, margins, and padding
+
 describe('Raster basic', function() {
   describe('a Mocha example', function() {
     it('the header should equal its text', function () {
-      var element = document.getElementById("test-header");
-      element.innerText.should.equal("A Test Header");
+
+      var contentElement = document.getElementById('content');
+      // var childNodes = contentElement.childNodes;
+      var childNodes = contentElement.querySelectorAll('*');
+
+      for (var i = childNodes.length - 1; i >= 0; --i) {
+      	var element = childNodes[i];
+        console.log("element.id = " + element.id);
+        console.log("element.clientHeight = " + element.clientHeight);
+        var style = window.getComputedStyle(element);
+        console.log("style.lineHeight = " + style.lineHeight);
+        console.log("style.marginBottom = " + style.marginBottom);
+
+      }
     });
   });
 });
