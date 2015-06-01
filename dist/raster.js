d64a4706dc3ffbedc21d13d1db8841d63c109a76
Roben Kleene <robenk@gmail.com>
2015-06-01 19:34:13 -0400

    Cleanup JavaScript

--- dist/raster.js
+++ dist/raster.js
@@ -1,24 +1,21 @@
 window.addEventListener('load', function () { setup(); }, false);
-function setup() {
-	// Baseline
-	var elements = document.getElementsByClassName("baseline_old");
-	for (var i=0; i < elements.length; i++) {
-		var element = elements[i];
-		var canvas = canvasForElement(element);
-        var lineHeight = window.getComputedStyle(element, null).getPropertyValue("line-height");
-		drawBaselineOnCanvas(canvas, lineHeight);
-	};
 
-	var elements = document.getElementsByClassName("baseline");
-	for (var i=0; i < elements.length; i++) {
+function setup() {
+	// // Baseline
+	// var elements = document.getElementsByClassName("baseline_old");
+	// for (var i=0; i < elements.length; i++) {
+	// 	var element = elements[i];
+	// 	var canvas = canvasForElement(element);
+  //       var lineHeight = window.getComputedStyle(element, null).getPropertyValue("line-height");
+	// 	drawBaselineOnCanvas(canvas, lineHeight);
+	// }
+
+	elements = document.getElementsByClassName("baseline");
+	for (var i = 0; i < elements.length; i++) {
 		var element = elements[i];
 		var container = containerForElement(element);
 		showBaseline(container);
-	};
-
-	// Grid
-	// var gridElements = document.getElementsByClassName("grid");
-	// for (var i=0; i < baselineElements.length; i++) {
+	}
 
 }
 
@@ -26,16 +23,13 @@ function showBaseline(container) {
 	container.style.height = "auto";
 	container.style.width = "100%";
 
-    var containerHeightInt = parseInt(window.getComputedStyle(container, null).getPropertyValue("height"));
+  var containerHeightInt = parseInt(window.getComputedStyle(container, null).getPropertyValue("height"));
 	var parentHeightInt = parseInt(window.getComputedStyle(container.parentNode, null).getPropertyValue("height"));
 
-
-	// container.style.backgroundColor = "rgba(255, 72, 147, 0.25)";
-	
 	while (containerHeightInt < parentHeightInt) {
 		// Add an element to contain the baseline
 		var baselineElement = document.createElement("p");
-		resetCSS(baselineElement)
+		resetCSS(baselineElement);
 		baselineElement.appendChild(document.createTextNode('\u00A0'));
 
 		// Draw the baseline on a canvas element
@@ -57,36 +51,11 @@ function showBaseline(container) {
 		baselineElement.appendChild(canvas);
 		container.appendChild(baselineElement);
 		containerHeightInt = parseInt(window.getComputedStyle(container, null).getPropertyValue("height"));
-		// console.log(containerHeightInt + " < " + parentHeightInt);
-	}
-}
-
-// Drawing
-function drawBaselineOnCanvas(canvas, lineHeight) {	
-	var context = contextForCanvas(canvas);
-	if (context) {
-		var width = canvas.offsetWidth;
-		var height = canvas.offsetHeight
-		
-		// Border
-		context.strokeStyle = "#f00";
-		context.lineWidth = 1;
-		context.strokeRect(0, 0, width, height);
-		// Baseline
-		context.strokeStyle = "#D7F1FF";
-		var leading = parseInt(lineHeight);
-		var lines = Math.floor(height / leading);
-		for (var i = 0; i < lines; i++) {
-			var y = (i + 1) * leading;
-			context.moveTo(0, y);
-			context.lineTo(width, y);
-			context.stroke();
-			context.closePath();
-		};
 	}
 }
 
 // Helpers
+
 function containerForElement(element) {
 	var container = document.createElement("div");
 	element.appendChild(container);
@@ -97,8 +66,6 @@ function containerForElement(element) {
 function makeContainer(element) {
 	var parent = element.parentNode;
 	parent.style.position = "relative";
-	
-	// console.log(parent + "width = " + parent.offsetWidth + " height = " + parent.offsetHeight);
 
 	var height = parent.offsetHeight;
 	var width = parent.offsetWidth;
@@ -110,22 +77,6 @@ function makeContainer(element) {
 	element.style.position = "absolute";
 	element.style.left = "0";
 	element.style.top = "0";
-
-	// console.log(element + "width = " + element.style.width + " height = " + element.style.height);
-}
-
-// Canvas Helpers
-function contextForCanvas(canvas) {
-	if (canvas && canvas.getContext) {
-		return canvas.getContext('2d');
-	}
-}
-
-function canvasForElement(element) {
-	var canvas = document.createElement("canvas");
-	element.appendChild(canvas);
-	makeContainer(canvas);
-	return canvas;
 }
 
 function resetCSS(element) {
@@ -137,10 +88,47 @@ function resetCSS(element) {
 	element.style.verticalAlign = "baseline";
 	element.style.background = "transparent";
 }
-// margin: 0;
-// padding: 0;
-// border: 0;
-// outline: 0;
-// font-size: 100%;
-// vertical-align: baseline;
-// background: transparent;
+
+
+
+// Canvas
+
+// function drawBaselineOnCanvas(canvas, lineHeight) {
+// 	var context = contextForCanvas(canvas);
+// 	if (context) {
+// 		var width = canvas.offsetWidth;
+// 		var height = canvas.offsetHeight;
+//
+// 		// Border
+// 		context.strokeStyle = "#f00";
+// 		context.lineWidth = 1;
+// 		context.strokeRect(0, 0, width, height);
+// 		// Baseline
+// 		context.strokeStyle = "#D7F1FF";
+// 		var leading = parseInt(lineHeight);
+// 		var lines = Math.floor(height / leading);
+// 		for (var i = 0; i < lines; i++) {
+// 			var y = (i + 1) * leading;
+// 			context.moveTo(0, y);
+// 			context.lineTo(width, y);
+// 			context.stroke();
+// 			context.closePath();
+// 		}
+// 	}
+// }
+
+// Canvas Helpers
+
+// function contextForCanvas(canvas) {
+// 	if (canvas && canvas.getContext) {
+// 		return canvas.getContext('2d');
+// 	}
+// }
+//
+// function canvasForElement(element) {
+// 	var canvas = document.createElement("canvas");
+// 	element.appendChild(canvas);
+// 	makeContainer(canvas);
+// 	return canvas;
+// }
+//
