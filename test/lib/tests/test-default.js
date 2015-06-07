4eb7739fdc34249e027a372118738571cdb99036
Roben Kleene <robenk@gmail.com>
2015-06-06 20:43:52 -0400

    Make more default style tests

--- test/lib/tests/test-default.js
+++ test/lib/tests/test-default.js
@@ -1,14 +1,24 @@
 describe('Raster default', function() {
+  var leadingRems = 1.25;
   describe('The default style', function() {
     it('the baseline should be correct with the default font size', function () {
       var contentElement = document.getElementById('content');
       var htmlElement = document.getElementsByTagName("html")[0];
       var wholeNumberFontSizes = ["8px", "12px", "16px", "20px"];
       for (var i = 0; i < wholeNumberFontSizes.length; i++) {
-        // TODO: Confirm that the font-size has changed by asserting the returned value of the style property
-        // TODO: Confirm that the font-size has changed by asserting the `clientHeight` of an element
+        // Set the font size on the HTML Element
+        var fontSize = wholeNumberFontSizes[i];
+        htmlElement.style.fontSize = fontSize;
 
-        htmlElement.style.fontSize = wholeNumberFontSizes[i];
+        // Assert that the font size has been set properly
+        htmlElement.style.fontSize.should.equal(fontSize);
+        var testElements = htmlElement.getElementsByTagName('li');
+        var testElement = testElements[0];
+        testElement.should.be.an('object');
+        var testClientHeight = parseInt(fontSize, 10) * leadingRems;
+        testElement.clientHeight.should.equal(testClientHeight);
+
+        // Run the baseline tests
         testHelper.baselineIsCorrect(contentElement);
       }
     });
