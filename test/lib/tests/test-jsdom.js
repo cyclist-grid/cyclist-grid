1e39a68f4d9c2f56627e59cbd62f524d9924dd7a
Roben Kleene <contact@robenkleene.com>
2021-07-15 10:52:40 +0000

    Setup JSDOM

--- test/lib/tests/test-jsdom.js
+++ test/lib/tests/test-jsdom.js
@@ -1,9 +1,8 @@
-var jsdom = require('mocha-jsdom')
-var assert = require('assert');
+const { JSDOM } = require('jsdom');
 describe('All Raster tests', function() {
   ["../../test-default.html"].forEach(file => {
     describe(file, function() {
-      jsdom.fromFile(file).then((dom) => {
+      JSDOM.fromFile(file).then((dom) => {
         dom.window.mocha.run();
       });
     });      
