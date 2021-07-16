9b704ad64eadb47a3b8a377300090be0b16ca662
Roben Kleene <contact@robenkleene.com>
2021-07-16 10:41:29 +0000

    Fix loading the path

--- test/lib/tests/test-jsdom.js
+++ test/lib/tests/test-jsdom.js
@@ -1,9 +1,10 @@
 const { JSDOM } = require('jsdom');
 describe('All Raster tests', function() {
-  ["../../test-default.html"].forEach(file => {
+  ["test-default.html"].forEach(file => {
+    const path = require('path').resolve(__dirname, '../../build/html', file);
     describe(file, function() {
       it('should run without error', function(done) {
-        JSDOM.fromFile(file).then((dom) => {
+        JSDOM.fromFile(path).then((dom) => {
           console.log("got here");
           dom.window.mocha.run();
         })
