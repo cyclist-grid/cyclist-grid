6339196d4df02e0dfafb2ec3b8d45bcdcc39cda1
Roben Kleene <contact@robenkleene.com>
2021-07-19 10:40:39 +0000

    Work on jsdom

--- test/lib/tests/test-jsdom.js
+++ test/lib/tests/test-jsdom.js
@@ -4,8 +4,8 @@ describe('All Raster tests', function() {
     const path = require('path').resolve(__dirname, '../../build/html', file);
     describe(file, function() {
       it('should run without error', function(done) {
-        JSDOM.fromFile(path, { resources: "usable" }).then((dom) => {
-          dom.window.mocha.run();
+        JSDOM.fromFile(path, { resources: "usable", runScripts: "dangerously", url: "http://localhost" }).then((dom) => {
+          // dom.window.mocha.run();
         })
         .then(done, done);  
       });
