7c10ed8df12578920c20eb38d10279ecfbfd0afb
Roben Kleene <contact@robenkleene.com>
2021-07-20 10:39:33 +0000

    Use correct paths

--- test/lib/tests/test-jsdom.js
+++ test/lib/tests/test-jsdom.js
@@ -1,10 +1,14 @@
 const { JSDOM } = require('jsdom');
+const path = require('path');
+const url = require('url');
 describe('All Raster tests', function() {
   ["test-default.html"].forEach(file => {
-    const path = require('path').resolve(__dirname, '../../build/html', file);
+    const basePath = path.resolve(__dirname, '../../build/html');
+    const filePath = path.resolve(basePath, file);
+    const baseFileURL = url.pathToFileURL(basePath);
     describe(file, function() {
       it('should run without error', function(done) {
-        JSDOM.fromFile(path, { resources: "usable", runScripts: "dangerously", url: "http://localhost" }).then((dom) => {
+        JSDOM.fromFile(filePath, { resources: "usable", runScripts: "dangerously", url: baseFileURL }).then((dom) => {
           // dom.window.mocha.run();
         })
         .then(done, done);  
