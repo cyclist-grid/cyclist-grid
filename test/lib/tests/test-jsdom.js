010f663b37c7879628752a949946744da459ecd2
Roben Kleene <contact@robenkleene.com>
2021-07-20 10:44:53 +0000

    Fix path

--- test/lib/tests/test-jsdom.js
+++ test/lib/tests/test-jsdom.js
@@ -5,7 +5,7 @@ describe('All Raster tests', function() {
   ["test-default.html"].forEach(file => {
     const basePath = path.resolve(__dirname, '../../build/html');
     const filePath = path.resolve(basePath, file);
-    const baseFileURL = url.pathToFileURL(basePath);
+    const baseFileURL = url.pathToFileURL(basePath) + '/';
     describe(file, function() {
       it('should run without error', function(done) {
         JSDOM.fromFile(filePath, { resources: "usable", runScripts: "dangerously", url: baseFileURL }).then((dom) => {
